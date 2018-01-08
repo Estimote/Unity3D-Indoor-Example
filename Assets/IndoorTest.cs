4b55c53c3adb76c79a75e675a1ecff879bc0d513
Piotr Krawiec <piotr@krawiec.me>
2018-01-08 14:05:10 +0100

    Initial commit

---
+++ Assets/IndoorTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class IndoorTest : MonoBehaviour
+{
+
+	public Text text;
+
+	public IndoorLocationManager indoorManager = IndoorLocationManagerFactory.Create ();
+
+	void Start ()
+	{
+		indoorManager.Start ("<APP ID>", "<APP TOKEN>", "<LOCATION IDENTIFIER>");
+	}
+
+	void Update ()
+	{
+		if (indoorManager.IsInsideLocation ()) {
+			double x = indoorManager.GetX ();
+			double y = indoorManager.GetY ();
+
+			text.text = string.Format ("x = {0:0.00}, y = {1:0.00}", x, y);
+		} else {
+			text.text = "outside location";
+		}
+	}
+}
